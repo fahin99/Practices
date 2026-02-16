fc0ee91e1d49e34ba053593787cfb5d9109c33ff
Kazi Fahin Abraj Abir <2405007@ugrad.cse.buet.ac.bd>
2026-02-17 00:52:55 +0600

    Implement topological sorting and cycle detection in course scheduling

--- dsa_prac/course_Schedule_toposort_dfs.cpp
+++ dsa_prac/course_Schedule_toposort_dfs.cpp
@@ -34,6 +34,48 @@ bool icanFinish(int numCourses, vector<vector<int>>& prerequisites) {
     return true;
 }
 
+void topoSortUtil(int v, vector<bool> &visited, stack<int> &st, vector<int> adj[]) {
+    visited[v] = true;
+    for (int neighbor : adj[v]) {
+        if (!visited[neighbor]) {
+            topoSortUtil(neighbor, visited, st, adj);
+        }
+    }
+    st.push(v);
+}
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> adj[numCourses];
+    for (const auto& prereq : prerequisites) {
+        adj[prereq[1]].push_back(prereq[0]);
+    }
+    
+    vector<bool> visited(numCourses, false);
+    vector<bool> recStack(numCourses, false);
+    stack<int> topoStack;
+    
+    for (int i = 0; i < numCourses; i++) {
+        if (!visited[i]) {
+            if (isCyclicUtil(i, visited, recStack, adj))
+                return {}; // Return empty if there's a cycle
+        }
+    }
+    // If no cycle, perform topological sort
+    fill(visited.begin(), visited.end(), false); // Reset visited for topological sort
+    for (int i = 0; i < numCourses; i++) {
+        if (!visited[i]) {
+            topoSortUtil(i, visited, topoStack, adj);
+        }
+    }
+    
+    vector<int> order;
+    while (!topoStack.empty()) {
+        order.push_back(topoStack.top());
+        topoStack.pop();
+    }
+    
+    return order;
+}
+
 int main() {
     int numCourses = 4;
     vector<vector<int>> prerequisites = {{1,0},{2,0},{3,1},{3,2}};
@@ -43,6 +85,13 @@ int main() {
     } else {
         cout << "Not all courses can be finished." << endl;
     }
+    
+    vector<int> order = findOrder(numCourses, prerequisites);
+    cout << "Course order: ";
+    for (int course : order) {
+        cout << course << " ";
+    }
+    cout << endl;
     
     return 0;
 }
